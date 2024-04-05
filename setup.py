218b1ed263068a0ab2bd01fc9464b1e9b7abccf4
yudi-lap <yudikarn@gmail.com>
2024-04-05 16:39:07 +0530

    template file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_namespace_packages
+
+setup(
+    name= 'DiamondPricePrediction',
+    version='0.0.1',
+    author= 'ujjwal das',
+    author_email='yudikarn@gmail.com',
+    packages=find_packages()
+)
