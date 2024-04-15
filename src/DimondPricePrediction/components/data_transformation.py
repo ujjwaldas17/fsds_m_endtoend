5126e03140f52bae1b0be797d7f3c95409c7a172
yudi-lap <yudikarn@gmail.com>
2024-04-15 12:30:07 +0530

    file updated

---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,9 @@
+
+
+class DataTransformation:
+    def__init__(self):
+     pass
+
+def get_data_transformation(self):
+    pass
+
---
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+class ModelTrainer:
+    def__init__(self):
+     pass
+
+def initiate_data_trainer(self):
+    pass
