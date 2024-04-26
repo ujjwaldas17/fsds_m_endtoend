570d30cf1320cde350612dccde9a44a50663fa40
yudi-lap <yudikarn@gmail.com>
2024-04-26 16:44:59 +0530

    python version changed

--- init_setup.sh
+++ init_setup.sh
@@ -4,7 +4,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.8 version" 
 
 
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./env python=3.12 -y
 
 
 echo [$(date)]: "activating the environment"
