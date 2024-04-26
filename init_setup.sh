cd7108b84e3bd2fb995485cc5d96af900e6c97fc
yudi-lap <yudikarn@gmail.com>
2024-04-26 15:55:00 +0530

    saw some cmds

--- init_setup.sh
+++ init_setup.sh
@@ -7,7 +7,7 @@ echo [$(date)]: "creating env with python 3.8 version"
 conda create --prefix ./env python=3.8 -y
 
 
-echo [$(date)]: "activating the environment" 
+echo [$(date)]: "activating the environment"
 
 source activate ./env
 
